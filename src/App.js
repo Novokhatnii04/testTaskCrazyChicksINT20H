41bb1244b6d25cedde7515b82b7fcc3f9dc9ba79
drDaria <ddrobazkina@gmail.com>
2024-02-08 23:58:53 +0200

    Squashed commit of the following:
    commit aa8b38ad94ad4e9c3b0f22f87234c8bdf3dcea2d
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Thu Feb 8 21:00:05 2024 +0200
    
        added more fixes
    
    commit e8af124585d882398f879f63bf9b2d78f5ad3402
    Merge: 5a0e378 1d06c8a
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Thu Feb 8 20:45:10 2024 +0200
    
        Merge branch 'Daria-frontend'
    
    commit 5a0e37808ef46f3349c394b728ff9713131a72aa
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Thu Feb 8 20:43:55 2024 +0200
    
        fixed font
    
    commit 9e659ef6f137b066c759b12710b493608404c11d
    Merge: 7967922 8e33405
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Thu Feb 8 20:39:49 2024 +0200
    
        Merge branch 'Daria-frontend'
    
    commit 7967922e7246a957afb5d7c337794ed7adcd6d57
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 16:45:10 2024 +0200
    
        again fixed footer
    
    commit 3035de7ee78644801d06430615cacf01498cdc4f
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 16:41:28 2024 +0200
    
        fixed footer
    
    commit 64764d63ef0976db204a51934a1d0f0088997fe1
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 16:39:43 2024 +0200
    
        Update auction.sass
    
    commit 7033144c63dd4d9820e5055b54b52b812e38baa3
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 16:34:14 2024 +0200
    
        finished cards menu
    
    commit 3dafd4aae56c2e895424408cd894a5df1e7c2f14
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 16:11:10 2024 +0200
    
        added cards auction
    
    commit 744241cde9e1500b42c7047f012e886c95f88ea8
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 15:14:03 2024 +0200
    
        Again fixed
    
    commit 78c5fe0f4adfd2f0185bac107f656ec3334ecfcb
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 15:12:04 2024 +0200
    
        Initial before merge
    
    commit b716b93a4047d7ce895d9f835923608b58b2a686
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 15:11:43 2024 +0200
    
        total fixed
    
    commit d7155aa63b01367607192b094c9c0b68bbe7dac3
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 14:54:10 2024 +0200
    
        Update stylesheet.css
    
    commit 1203e05c8fe3c527de866f6aeeefa2080cf497a8
    Merge: ff8418d da4f9b0
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 14:53:52 2024 +0200
    
        Merge branch 'Daria-frontend' into vladyk_frontend
    
    commit ff8418ddc6fbe1f78e40199a962bba9c9136c2bf
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 14:51:52 2024 +0200
    
        added card
    
    commit e70d2ec52f5367bee7f05c07e21f412c0aca4347
    Author: Владислав Новохатній <108412890+Novokhatnii04@users.noreply.github.com>
    Date:   Tue Feb 6 14:45:31 2024 +0200
    
        begin to create cards menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuctionCards from "./pages/AuctionCards/auction";
 import MainPage from "./pages/Main/Main";
 import Slot from "./pages/Slot/Slot";
 import Footer from "./pages/Footer/footer";
+import "./App.css";
 
 const App = () => {
-   return (
-      <Router>
-         <Routes>
-            <Route exact path="/" element={<MainPage />} />
-            <Route path="/slot" element={<Slot />} />
-         </Routes>
-         <Footer />
-      </Router>
-   );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/slot" element={<Slot />} />
+        <Route path="/auction" element={<AuctionCards />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
